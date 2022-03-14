f6b822db1cc0230546504ffc0925378ff0810874
seema-khandelwal <seemakhandelwal269@gmail.com>
2022-03-14 13:04:18 +0530

    increment

---
+++ Increment.java
@@ -0,0 +1,15 @@
+package com.te.javabasic.coommon;
+
+public class Increment {
+ public static void main(String[] args) {
+	int a=0;
+	a=++a;
+	System.out.println(a);
+	a=++a;
+	System.out.println(a);
+	a=a++;
+	System.out.println(a);
+	a=a++;
+	System.out.println(a);
+}
+}
